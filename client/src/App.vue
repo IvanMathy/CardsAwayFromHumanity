26c8c4291b50f3168c561fc8b9e316fbf58f6a5e
Ivan Mathy <IvanMathy@users.noreply.github.com>
2020-04-05 13:51:15 -0500

    Socket io setup

--- client/src/App.vue
+++ client/src/App.vue
@@ -1,6 +1,5 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
     <HelloWorld msg="Welcome to Your Vue.js + TypeScript App"/>
   </div>
 </template>
@@ -18,12 +17,13 @@ export default class App extends Vue {}
 </script>
 
 <style lang="scss">
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
+body {
+  font-family: Helvetica Neue, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
-  color: #2c3e50;
+  color: white;
   margin-top: 60px;
+  background-color: #242424;
 }
 </style>
--- server/app/app.ts
+++ server/app/app.ts
@@ -1,1 +1,9 @@
-console.log("Hello  World.")
+import io from "socket.io";
+
+const server = io.listen(3000);
+
+server.on("connection", function(socket) {
+  console.log("user connected");
+  socket.emit("welcome", "welcome man");
+});
+
